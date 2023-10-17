8e0e366f9616e655c73820ac6e207cb64685ba26
Shashank Mittal <latashashank.3@gmail.com>
2023-10-17 23:45:34 +0530

    structAndMethod problems with tests added
    Signed-off-by: Shashank Mittal <latashashank.3@gmail.com>

--- tasks/structAndMethod.go
+++ tasks/structAndMethod.go
@@ -1,1 +1,39 @@
 package tasks
+
+// Define a struct named Person with fields Name, Age, and City
+type Person struct {
+	Name string
+	Age  int
+	City string
+}
+
+// Task 1: Define a Struct and Print Its Fields
+
+// Write a function named DefineAndPrintStruct that defines a struct named Person with the following fields:
+// Name (string)
+// Age (int)
+// City (string)
+// Then print its fields.
+
+func DefineAndPrintStruct() {
+	// Define a struct named Person with fields Name, Age, and City , Again.
+
+	// Create an instance of the Person struct with sample values.
+	person := Person{
+		Name: "John",
+		Age:  30,
+		City: "New York",
+	}
+	// Print the fields of the struct instance.
+}
+
+// Task 2: Add a Method to a Struct
+// Extend the Person struct from Task 6 with a method named GetInfo that takes no arguments and returns a string. This method should return a formatted string that includes the person's name, age, and city.
+
+// Construct the GetInfo method such that it takes a struct of type Person
+// and returns a string.
+
+// For example, if the person's name is "Alice," age is 25, and city is "New York,"
+// the GetInfo method should return "Name: Alice, Age: 25, City: New York."
+
+// here
--- tests/structAndMethod_test.go
+++ tests/structAndMethod_test.go
@@ -1,1 +1,38 @@
 package tests
+
+import (
+	"ishashankmittal/go_assignment/tasks"
+	"testing"
+)
+
+type TestPerson struct {
+	tasks.Person
+}
+
+func TestDefineAndPrintStruct(t *testing.T) {
+	// Test case for defining and printing the Person struct
+	capturedOutput := captureOutput(func() {
+		tasks.DefineAndPrintStruct()
+	})
+
+	expectedOutput := "Name: John, Age: 30, City: New York\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+}
+
+func TestAddMethodToStruct(t *testing.T) {
+	// Create a Person instance with sample values
+	testperson := TestPerson{
+		Person: tasks.Person{Name: "Alice",
+			Age:  25,
+			City: "New York"},
+	}
+
+	// Test the GetInfo method
+	info := testperson.Person.GetInfo()
+	expectedInfo := "Name: Alice, Age: 25, City: New York"
+	if info != expectedInfo {
+		t.Errorf("Expected info: %s, Got: %s", expectedInfo, info)
+	}
+}
