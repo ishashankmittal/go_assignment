713debb0f0010b30b1576de899241fe88f26570c
Shashank Mittal <latashashank.3@gmail.com>
2023-10-18 00:05:11 +0530

    goroutines task added.

--- tasks/goroutines.go
+++ tasks/goroutines.go
@@ -1,1 +1,11 @@
 package tasks
+
+// Task: Print Numbers 1 to 10 Using Goroutines
+
+// Write a Go program that uses goroutines to print numbers from 1 to 10. The program should do the following:
+
+// Create two goroutines, printFirstHalf and printSecondHalf, that print numbers from 1 to 5 and numbers from 6 to 10, respectively.
+
+// Use goroutines to ensure that the numbers are printed concurrently.
+
+// Print the numbers in ascending order with a 1-second delay between each number.
