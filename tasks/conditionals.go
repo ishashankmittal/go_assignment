844ef15e50953808969395ba100f1ff5594bf237
Shashank Mittal <latashashank.3@gmail.com>
2023-10-17 23:01:58 +0530

    conditionals problems with tests added.
    Signed-off-by: Shashank Mittal <latashashank.3@gmail.com>

--- tasks/conditionals.go
+++ tasks/conditionals.go
@@ -1,7 +1,28 @@
 package tasks
 
-// Task 12: Write a function that takes a boolean value and prints it in a user-friendly message.
+// Task 1: Write a function that takes an integer and prints a message based on the value:
+// - If the integer is positive, print "Positive."
+// - If the integer is negative, print "Negative."
+// - If the integer is zero, print "Zero."
 
-func PrintBooleanMessage(isValid bool) {
-    // Implement the code to print a message like "Is valid: [true/false]" based on the value of isValid.
+func PrintMessageBasedOnValue(num int) {
+	// Implement the code to print the appropriate message based on the value of num.
+}
+
+// Task 2: Write a function that takes a string and checks if it is a valid day of the week.
+// - If the input is "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday",
+//   print "Weekday."
+// - If the input is "Saturday" or "Sunday", print "Weekend."
+// - If the input is anything else, print "Invalid day."
+
+func CheckDayOfWeek(input string) {
+	// Implement the code to check the validity of the day and print the corresponding message.
+}
+
+// Task 4: Write a function that takes a map of string keys and integer values and calculates the sum of the values.
+// If the sum of values is greater than 10, print "Sum is greater than 10."
+// If the sum of values is less than or equal to 10, print "Sum is 10 or less."
+
+func CheckIfSumIsLessThan10(inputMap map[string]int) {
+	// Implement the code.
 }
--- tests/conditionals_test.go
+++ tests/conditionals_test.go
@@ -1,1 +1,102 @@
 package tests
+
+import (
+	"ishashankmittal/go_assignment/tasks"
+	"testing"
+)
+
+func TestPrintMessageBasedOnValue(t *testing.T) {
+	// Test case for a positive number
+	capturedOutput := captureOutput(func() {
+		tasks.PrintMessageBasedOnValue(5)
+	})
+
+	expectedOutput := "Positive\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For 5, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+
+	// Test case for a negative number
+	capturedOutput = captureOutput(func() {
+		tasks.PrintMessageBasedOnValue(-5)
+	})
+
+	expectedOutput = "Negative\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For -5, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+
+	// Test case for zero
+	capturedOutput = captureOutput(func() {
+		tasks.PrintMessageBasedOnValue(0)
+	})
+
+	expectedOutput = "Zero\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For 0, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+}
+
+func TestCheckDayOfWeek(t *testing.T) {
+	// Test case for a weekday
+	capturedOutput := captureOutput(func() {
+		tasks.CheckDayOfWeek("Monday")
+	})
+
+	expectedOutput := "Weekday\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For Monday, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+
+	// Test case for a weekend day
+	capturedOutput = captureOutput(func() {
+		tasks.CheckDayOfWeek("Saturday")
+	})
+
+	expectedOutput = "Weekend\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For Saturday, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+
+	// Test case for an invalid day
+	capturedOutput = captureOutput(func() {
+		tasks.CheckDayOfWeek("InvalidDay")
+	})
+
+	expectedOutput = "Invalid day\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("For InvalidDay, Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+}
+
+func TestCheckIfSumIsLessThan10(t *testing.T) {
+	// Test case for a sum greater than 10
+	inputMap := map[string]int{
+		"A": 5,
+		"B": 6,
+	}
+
+	capturedOutput := captureOutput(func() {
+		tasks.CheckIfSumIsLessThan10(inputMap)
+	})
+
+	expectedOutput := "Sum is greater than 10\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+
+	// Test case for a sum equal to 10
+	inputMap = map[string]int{
+		"A": 5,
+		"B": 5,
+	}
+
+	capturedOutput = captureOutput(func() {
+		tasks.CheckIfSumIsLessThan10(inputMap)
+	})
+
+	expectedOutput = "Sum is 10 or less\n"
+	if capturedOutput != expectedOutput {
+		t.Errorf("Expected output: %s, Got: %s", expectedOutput, capturedOutput)
+	}
+}
