f9f4556e01d190e222949538ccd7a8615e044f11
iitian_adi05tya <131003170+iitian-adi05tya@users.noreply.github.com>
2023-10-19 13:42:42 +0530

    Updated syntaxPractice.go

--- tasks/syntaxPractice.go
+++ tasks/syntaxPractice.go
@@ -12,9 +12,8 @@ func PrintHelloWorld() {
 
 // Task 2: Write a function that takes an integer and prints it to the console using string formatting.
 func PrintInteger(n int) {
-	var num int
-	fmt.Scanln(&num)
-	fmt.Println(num)
+	fmt.Scanln(&n)
+	fmt.Println(n)
 	// Implement the code to print the integer n.
 }
 
@@ -22,31 +21,19 @@ func PrintInteger(n int) {
 
 func PrintSum(a, b int) {
 	// Implement the code to print the sum of a and b.
-	var num1, num2 int
-	fmt.Println(num1 + num2)
+	fmt.Println(a + b)
 }
 
 // Task 4: Write a function that prints a message with placeholders for a name and age.
 
 func PrintMessageWithPlaceholders(name string, age int) {
 	// Implement the code to print a message like "Hello, my name is [name] and I am [age] years old."
-	fmt.Print("Hello, my name is ")
-	fmt.Print(name)
-	fmt.Print(" and I am ")
-	fmt.Print(age)
-	fmt.Print(" years old.")
-	fmt.Println()
-
-}
+	fmt.Print("Hello, my name is " ,name," and I am ",age," years old." )
+	}
 
 // Task 5: Write a function that prints a formatted date in the "YYYY-MM-DD" format.
 
 func PrintFormattedDate(year, month, day int) {
 	// Implement the code to print the date in "YYYY-MM-DD" format.
-	fmt.Println("The Date is ")
-	fmt.Print(year)
-	fmt.Print("-")
-	fmt.Print(month)
-	fmt.Print("-")
-	fmt.Print(day)
+	fmt.Println("The Date is ",year,"-",month,"-",day)
 }
