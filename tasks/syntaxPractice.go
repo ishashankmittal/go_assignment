85ddf8780f94f0ebb4a6ad07fb45a284be8474f7
Aksgo <146610691+Aksgo@users.noreply.github.com>
2023-10-19 02:02:33 +0000

    message

--- tasks/syntaxPractice.go
+++ tasks/syntaxPractice.go
@@ -12,23 +12,27 @@ func PrintHelloWorld() {
 
 func PrintInteger(n int) {
 	// Implement the code to print the integer n.
-	
+	fmt.Printf("%d",n)
 }
 
 // Task 3: Write a function that takes two integers, adds them, and prints the result.
 
 func PrintSum(a, b int) {
 	// Implement the code to print the sum of a and b.
+	result := a+b
+	fmt.Println(result)
 }
 
 // Task 4: Write a function that prints a message with placeholders for a name and age.
 
 func PrintMessageWithPlaceholders(name string, age int) {
 	// Implement the code to print a message like "Hello, my name is [name] and I am [age] years old."
+	fmt.printf("Hello my name is %s and I am %d years old.",name,age)
 }
 
 // Task 5: Write a function that prints a formatted date in the "YYYY-MM-DD" format.
 
 func PrintFormattedDate(year, month, day int) {
 	// Implement the code to print the date in "YYYY-MM-DD" format.
+	fmt.Printf("%d-%d-%d",year,month,day)
 }
