dfa05fb712adb47298262a0d2e4cc7f8ca53c29a
iitian_adi05tya <131003170+iitian-adi05tya@users.noreply.github.com>
2023-10-19 13:44:49 +0530

    Update main.go, removed the changes

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import "ishashankmittal/go_assignment/tasks"
 
 func main() {
 	tasks.PrintHelloWorld()
-	tasks.PrintMessageWithPlaceholders("Aaditya", 20)
-	tasks.PrintFormattedDate(2023, 10, 10)
 	tasks.PrintInteger(100)
-	tasks.PrintSum(10, 101)
+	tasks.PrintSum(10,101)
 }
