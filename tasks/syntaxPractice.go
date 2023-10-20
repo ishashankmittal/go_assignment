0ec229961c0a67e6677db43abeaecd9ae6258826
aayush0325 <ayush25khanna@gmail.com>
2023-10-20 11:21:52 +0530

    commit 3

--- tasks/syntaxPractice.go
+++ tasks/syntaxPractice.go
@@ -1,29 +1,31 @@
 package tasks
-import fmt
+import "fmt"
+
+
 // Task 1: Write a function that prints "Hello World" to the console.
 
 func PrintHelloWorld() {
 	// Implement the code to print "Hello World"
-	fmt.print("Hello World")
+	fmt.Print("Hello World")
 }
 
 // Task 2: Write a function that takes an integer and prints it to the console using string formatting.
 
 func PrintInteger(n int) {
-	// Implement the code to print the integer n.
+	fmt.Print(n)
 }
 
 // Task 3: Write a function that takes two integers, adds them, and prints the result.
 
 func PrintSum(a, b int) {
-	// Implement the code to print the sum of a and b.
+	fmt.Print
 }
 
 // Task 4: Write a function that prints a message with placeholders for a name and age.
 
 func PrintMessageWithPlaceholders(name string, age int) {
 	// Implement the code to print a message like "Hello, my name is [name] and I am [age] years old."
-}
+	
 
 // Task 5: Write a function that prints a formatted date in the "YYYY-MM-DD" format.
 
