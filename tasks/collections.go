b811d942d24d493ffb7ad08dda7f6b4a6e1fb6ad
aayush0325 <ayush25khanna@gmail.com>
2023-10-25 08:44:05 +0530

    Working from home

--- tasks/collections.go
+++ tasks/collections.go
@@ -1,21 +1,31 @@
 package tasks
 
+import "fmt"
+
 // Task 1: Write a function that creates and prints a slice of integers with values 1 to 5.
 
 func CreateAndPrintSlice() {
-	// Implement the code to create and print a slice of integers.
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
 }
 
 // Task 2: Write a function that takes a slice of integers and calculates and prints their sum.
 
 func CalculateAndPrintSum(numbers []int) {
-	// Implement the code to calculate and print the sum of the numbers in the slice.
+	sum := 0
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[i]
+	}
+	fmt.Println("Sum:", sum)
 }
 
 // Task 3: Write a function that creates and prints an array of strings containing days of the week.
 
 func CreateAndPrintDaysOfWeek() {
-	// Implement the code to create and print an array of strings containing days of the week.
+	days := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	for i := 0; i < len(days); i++ {
+		fmt.Println(days[i])
+	}
 }
 
 // Task 4: Write a function that takes a slice of integers, filters out even numbers, and prints the remaining odd numbers.
